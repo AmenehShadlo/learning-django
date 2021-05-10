610eb6077247907fab40a5d9352dd5fd9659cd86
Ameneh Shadlo <83941290+AmenehShadlo@users.noreply.github.com>
2021-05-10 21:11:23 +0430

    Delete Concert directory

--- Concert/concert/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from accounts import views
-
-
-urlpatterns = [
-    path('login/', views.loginView,name="login"),
-    path('logout/', views.logoutView),
-]
--- Concert/concert/ticketSales/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TicketsalesConfig(AppConfig):
-    name = 'ticketSales'
-    verbose_name="فروش بلیط کنسرت"
