0862f7235c40ca6b4bd4070d077e2b4c5ce064ee
ameneh <mnhshadloo@gmail.com>
2021-05-10 21:03:53 +0430

    q

--- Concert/concert/concert/settings.py
+++ Concert/concert/concert/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
